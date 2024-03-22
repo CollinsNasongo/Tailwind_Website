dbe75454cb37ecf756641c0e73bf1e38de57a46d
CollinsNasongo <nasongocollins@gmail.com>
2024-03-22 12:55:34 +0300

    Adding Side Menu

--- devcher/tailwind.config.js
+++ devcher/tailwind.config.js
@@ -4,7 +4,22 @@ module.exports = {
 
   content: ['./build/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes:{
+        'slideIn':{
+          '0%': {transform: 'translateX(100%)'},
+          '100%': { transform: 'translateX(0)' },
+        },
+        'slideOut':{
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(100%)' },
+        }
+      },
+      animation: {
+        'slideIn': 'slideIn 0.5s ease-in forwards',
+        'slideOut': 'slideOut 0.5s ease-out forwards',
+      },
+    },
   },
   plugins: [],
 }
